5d609c1e0cb0bfd919c3372f8976f7d5c1ade463
extoplasm <pleassehelsp@gmail.com>
2025-05-12 19:30:46 +1000

    fix: fixed a stage which was incorrect for testing purposes

--- src/components/pomodoro.tsx
+++ src/components/pomodoro.tsx
@@ -1,7 +1,7 @@
 import Timer from '@/components/timer'
 import { useState } from 'react'
 
-const stageMap = [1500, 3, 1500, 300, 1500, 300, 1500, 900]
+const stageMap = [1500, 300, 1500, 300, 1500, 300, 1500, 900]
 
 export default function Pomodoro() {
   const [isCountdownStarted, setCountdownStarted] = useState(false)
